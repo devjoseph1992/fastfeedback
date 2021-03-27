e19022ccb19dbe6b691e26740f3c1f5b4c7b07be
John Joseph Florina <devjosephflorina@gmail.com>
2021-03-27 22:55:49 +0800

    fixing firestore adding chakra ui

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,26 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
-
-import { auth } from "firebase";
 import { useAuth } from "../lib/auth";
 
 export default function Home() {
  const auth = useAuth();
 
  return (
-  <div className={styles.container}>
+  <div>
    <Head>
     <title>Fast Feedback</title>
-    <link rel="icon" href="/favicon.ico" />
    </Head>
+   <main>
+    <h1>fast feedback</h1>
 
-   <main className={styles.main}>
-    <h1 className={styles.title}>fast feedback</h1>
-
-    <p className={styles.description}>
+    <p>
      Current user:<code>{auth.user ? auth.user.email : "None"}</code>
     </p>
     {auth.user ? (
-     <button onClick={(e) => auth.signout()}></button>
+     <button onClick={(e) => auth.signout()}>Sign out</button>
     ) : (
-     <button onClick={(e) => auth.signinWithGithub()}></button>
+     <button onClick={(e) => auth.signinWithGitHub()}>Sign in</button>
     )}
    </main>
-
-   <footer className={styles.footer}>
-    <a
-     href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-     target="_blank"
-     rel="noopener noreferrer"
-    >
-     Powered by{" "}
-     <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-    </a>
-   </footer>
   </div>
  );
 }
